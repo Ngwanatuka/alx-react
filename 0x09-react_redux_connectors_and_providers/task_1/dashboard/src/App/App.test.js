c7e606d9c2a8232e979e86fda852973e9a19872a
Ngwanatuka <ngwanatuka@gmail.com>
2023-12-05 16:21:25 +0200

    updated the App.test to match the App.js

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
@@ -8,96 +8,101 @@ import Footer from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
 import { mapStateToProps } from "./App";
 import { fromJS } from "immutable";
+import configureStore from "redux-mock-store";
+import { displayNotificationDrawer, hideNotificationDrawer } from "../actions/uiActionCreators";
+
+const mockStore = configureStore();
 
 describe("App", () => {
+  let store;
+  let wrapper;
+
+  beforeEach(() => {
+    store = mockStore({
+      ui: {
+        isLoggedIn: false,
+        isNotificationDrawerVisible: false,
+      },
+    });
+
+    wrapper = shallow(<App store={store} />).dive(); // Dive into the connected component
+  });
+
   it("renders without crashing", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.exists()).toBe(true);
   });
 
   it("renders a div with the class App-header", () => {
-    const wrapper = shallow(<App />);
     const headerDiv = wrapper.find(".App-header");
     expect(headerDiv.exists()).toBe(true);
   });
 
   it("renders a div with a class of App-body", () => {
-    const wrapper = shallow(<App />);
     const bodyDiv = wrapper.find(".App-body");
     expect(bodyDiv.exists()).toBe(true);
   });
 
   it("renders a div with a class of App-footer", () => {
-    const wrapper = shallow(<App />);
     const footerDiv = wrapper.find(".App-footer");
     expect(footerDiv.exists()).toBe(true);
   });
 
   it("contains the Notification component", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Notifications).exists()).toBe(true);
   });
 
   it("contains the Header component", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Header).exists()).toBe(true);
   });
 
   it("contains the Login component", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Login).exists()).toBe(true);
   });
 
   it("contains the Footer component", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Footer).exists()).toBe(true);
   });
 
   it("does not display the CourseList component", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(CourseList).exists()).toBe(false);
   });
 
   it("displays the Login component when isLoggedIn is false", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Login)).toHaveLength(1);
     expect(wrapper.find(CourseList)).toHaveLength(0);
   });
 
   describe("when isLoggedIn is true", () => {
     it("does not display the Login component", () => {
-      const wrapper = shallow(<App />);
-      wrapper.setState({ user: { isLoggedIn: true } });
+      store = mockStore({
+        ui: {
+          isLoggedIn: true,
+          isNotificationDrawerVisible: false,
+        },
+      });
+      wrapper = shallow(<App store={store} />).dive();
+
       expect(wrapper.find(Login)).toHaveLength(0);
     });
 
     it("displays the CourseList component", () => {
-      const wrapper = shallow(<App />);
-      wrapper.setState({ user: { isLoggedIn: true } });
+      store = mockStore({
+        ui: {
+          isLoggedIn: true,
+          isNotificationDrawerVisible: false,
+        },
+      });
+      wrapper = shallow(<App store={store} />).dive();
+
       expect(wrapper.find(CourseList)).toHaveLength(1);
     });
   });
 
   it("should have default state for displayDrawer as false", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.state("displayDrawer")).toBe(false);
-  });
-
-  it("should update displayDrawer state to true after calling handleDisplayDrawer", () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().handleDisplayDrawer();
-    expect(wrapper.state("displayDrawer")).toBe(true);
-  });
-
-  it("should update displayDrawer state to false after calling handleHideDrawer", () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().handleDisplayDrawer();
-    wrapper.instance().handleHideDrawer();
     expect(wrapper.state("displayDrawer")).toBe(false);
   });
 
   it("should update user state after calling logIn function", () => {
-    const wrapper = shallow(<App />);
     const email = "test@example.com";
     const password = "password123";
     wrapper.instance().logIn(email, password);
@@ -109,7 +114,6 @@ describe("App", () => {
   });
 
   it("should update user state after calling logOut function", () => {
-    const wrapper = shallow(<App />);
     wrapper.instance().logOut();
     expect(wrapper.state("user")).toEqual({
       email: "",
@@ -119,8 +123,6 @@ describe("App", () => {
   });
 
   it("should mark notification as read", () => {
-    const wrapper = shallow(<App isLoggedIn={true} />);
-
     // Mock list of notifications
     const mockNotifications = [
       { id: 1, message: "Notification 1" },
@@ -140,24 +142,41 @@ describe("App", () => {
       { id: 3, message: "Notification 3" },
     ]);
   });
-});
 
-describe("mapStateToProps", () => {
-  it("returns the right object when passing the state", () => {
-    // Mock state using Immutable Map
-    const state = fromJS({
-      uiReducer: {
+  describe("mapStateToProps", () => {
+    it("returns the right object when passing the state", () => {
+      // Mock state using Immutable Map
+      const state = fromJS({
+        ui: {
+          isLoggedIn: true,
+          isNotificationDrawerVisible: true,
+        },
+        
+      });
+
+      // Call mapStateToProps with the mock state
+      const props = mapStateToProps(state);
+
+      // Verify that the function returns the correct object
+      expect(props).toEqual({
         isLoggedIn: true,
-      },
-      // Add other reducers if needed
+        isNotificationDrawerVisible: true,
+      });
     });
+  });
 
-    // Call mapStateToProps with the mock state
-    const props = mapStateToProps(state);
+  // Additional tests for mapDispatchToProps
+  it("maps displayNotificationDrawer action to props", () => {
+    const dispatchMock = jest.fn();
+    const mappedProps = mapDispatchToProps(dispatchMock);
+    mappedProps.displayNotificationDrawer();
+    expect(dispatchMock).toHaveBeenCalledWith(displayNotificationDrawer());
+  });
 
-    // Verify that the function returns the correct object
-    expect(props).toEqual({
-      isLoggedIn: true,
-    });
+  it("maps hideNotificationDrawer action to props", () => {
+    const dispatchMock = jest.fn();
+    const mappedProps = mapDispatchToProps(dispatchMock);
+    mappedProps.hideNotificationDrawer();
+    expect(dispatchMock).toHaveBeenCalledWith(hideNotificationDrawer());
   });
 });
