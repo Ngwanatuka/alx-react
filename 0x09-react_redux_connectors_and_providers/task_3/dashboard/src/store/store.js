55c5b41967a09d626d29a8f8cf21ac2871797251
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-04 21:12:48 +0200

    Add redux-devtools-extension to package.json and update store.js

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/store/store.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/store/store.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import uiReducer from "../reducers/uiReducer";
 
 const rootReducer = combineReducers({
   ui: uiReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
