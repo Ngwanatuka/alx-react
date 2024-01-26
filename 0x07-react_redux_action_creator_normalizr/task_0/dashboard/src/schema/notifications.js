dcff623442313cfcdbe71907c54018bdc7e0cb47
Ngwanatuka Molepo <111242056+Ngwanatuka@users.noreply.github.com>
2024-01-27 01:55:03 +0200

    Delete 0x07-react_redux_action_creator_normalizr directory

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,5 +0,0 @@
-import * as notificationData from '../../notifications.json';
-
-export function getAllNotificationsByUser(userId) {
-    return notificationData.filter(notification => notification.authore === userId);
-}
