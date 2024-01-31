a2246383a6ccc463a39a7acb7de0d6b80fb87d15
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-31 19:23:47 +0200

    Remove unnecessary files and code

--- 0x06-React_Immutable/2-nested.js
+++
@@ -1,7 +0,0 @@
-import { fromJS } from 'immutable';
-
-export default function accessImmutableObject(object, array) {
-  const immutableMap = fromJS(object);
-
-  return immutableMap.getIn(array, undefined);
-}
--- 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionTypes.js
+++
@@ -1,8 +0,0 @@
-export const MARK_AS_READ = 'MARK_AS_READ';
-export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
-
-// Constants for the type filters
-export const NotificationTypeFilters = {
-    DEFAULT: 'DEFAULT',
-    URGENT: 'URGENT',
-};
