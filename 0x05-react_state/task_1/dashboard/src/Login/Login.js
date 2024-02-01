3e69b5b6a5b0c0bd6f40f40f62169dca9df5812d
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-02 01:00:41 +0200

    added useEffect and enableSubmit if the two inputs are empty

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,23 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, css } from "aphrodite";
 
 function Login() {
-  const [isLoggin, setIsLoggin] = useState(false);
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [enableSubmit, setEnableSubmit] = useState(false);
+
+  useEffect(() => {
+    if (inputs.email !== "" && inputs.password !== "") {
+      setEnableSubmit(true);
+    } else {
+      if (enableSubmit !== false) setEnableSubmit(false);
+    }
+  } , [inputs.email, inputs.password]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setIsLoggin(true);
+    setIsLoggedIn((prev) => ({ ...prev, isLoggedIn: true }));
   };
+
+  const handleChangeEmail = (e) => {
+    e.persist();
+    setInputs((prev) => ({ ...prev, email: e.target.value }));
+  };
+
+  const handleChangePassword = (e) => {
+    e.persist();
+    setInputs((prev) => ({ ...prev, password: e.target.value }));
+  };
+
   return (
     <div className={css(styles.appBody)}>
       <p>Login to access the full dashboard</p>
       <form onSubmit={handleSubmit}>
         <div className={css(styles.appLogin)}>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={inputs.email}
+            onChange={handleChangeEmail}
+          />
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" />
-          <input type="submit" className={css(styles.loginButton)} value="OK" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={inputs.password}
+            onChange={handleChangePassword}
+          />
+          <input type="submit" className={css(styles.loginButton)} value="OK" disabled={!enableSubmit}/>
         </div>
       </form>
     </div>
@@ -51,8 +87,14 @@ const styles = StyleSheet.create({
   loginButton: {
     backgroundColor: "white",
     border: "1px solid #000",
-    borderRadius: "5px",
+    borderRadius: "2px",
     boxShadow: "0 0 1px 1px #000",
+    cursor: "pointer",
+    transition: "all 0.3s ease-in-out",
+    ":hover": {
+      backgroundColor: "#000",
+      color: "#fff",
+    },
     "@media (max-width: 900px)": {
       marginTop: "1.5%",
       width: "15%",
