6eecb9d2d735d3d1504df040447d37f70417e58c
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-01 16:22:52 +0200

    Add login functionality to Login component

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, css } from "aphrodite";
 
 function Login() {
-    return (
+  const [isLoggin, setIsLoggin] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoggin(true);
+  };
+  return (
     <div className={css(styles.appBody)}>
       <p>Login to access the full dashboard</p>
-      <div className={css(styles.appLogin)}>
-        <label htmlFor="email">Email:</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" />
-        <button className={css(styles.loginButton)}>OK</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className={css(styles.appLogin)}>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" />
+          <input type="submit" className={css(styles.loginButton)} value="OK" />
+        </div>
+      </form>
     </div>
   );
 }
@@ -19,37 +27,35 @@ function Login() {
 export default Login;
 
 const styles = StyleSheet.create({
-    appBody: {
-      fontFamily: "Poppins, sans-serif",
-      color: "#000000",
-      overflow: "none",
-    },
-    paragraph: {
-      fontWeight: 600,
-      marginTop: "5%",
-      marginLeft: "5%",
-    },
-    appLogin: {
+  appBody: {
+    fontFamily: "Poppins, sans-serif",
+    color: "#000000",
+    overflow: "none",
+  },
+  paragraph: {
+    fontWeight: 600,
+    marginTop: "5%",
+    marginLeft: "5%",
+  },
+  appLogin: {
+    display: "flex",
+    gap: "1.5%",
+    marginLeft: "5%",
+    "@media (max-width: 900px)": {
       display: "flex",
-      gap: "1.5%",
-      marginLeft: "5%",
-      '@media (max-width: 900px)': {
-        display: 'flex',
-        flexDirection: 'column',
-        border: 'none',
-        width: '35%',
-      },
+      flexDirection: "column",
+      border: "none",
+      width: "35%",
     },
-    loginButton: {
-      backgroundColor: "white",
-      border: "1px solid #000",
-      borderRadius: "5px",
-      boxShadow: "0 0 1px 1px #000",
-      '@media (max-width: 900px)': {
-        marginTop: '1.5%',
-        width: '15%',
-      },
+  },
+  loginButton: {
+    backgroundColor: "white",
+    border: "1px solid #000",
+    borderRadius: "5px",
+    boxShadow: "0 0 1px 1px #000",
+    "@media (max-width: 900px)": {
+      marginTop: "1.5%",
+      width: "15%",
     },
-  });
-
-
+  },
+});
