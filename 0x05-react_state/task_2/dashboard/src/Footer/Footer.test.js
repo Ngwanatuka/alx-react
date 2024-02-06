ee38c34e3d844ee882dd516e47960c7713112a77
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-06 18:09:14 +0200

    Add contact link to Footer component

--- 0x05-react_state/task_2/dashboard/src/Footer/Footer.test.js
+++ 0x05-react_state/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Footer from "./Footer";
 import { shallow } from "enzyme";
+import { AppContext } from "../App/AppContext";
 
 describe("Footer component", () => {
   it("renders without crashing", () => {
     const wrapper = shallow(<Footer />);
-    expect(wrapper.exists());
+    expect(wrapper.exists()).toBe(true);
   });
 
-  it('renders that text "Copyright"', () => {
+  it('renders the text "Copyright"', () => {
     const wrapper = shallow(<Footer />);
-    expect(wrapper.contains("Copyright"));
+    expect(wrapper.text()).toContain("Copyright");
+  });
+
+  it("does not display the contact link when user is logged out", () => {
+    const wrapper = shallow(
+      <AppContext.Provider value={{ user: { isLoggedIn: false } }}>
+        <Footer />
+      </AppContext.Provider>
+    );
+    expect(wrapper.find("a[href='/contact']").exists()).toBe(false);
+  });
+
+  it("displays the contact link when user is logged in", () => {
+    const wrapper = shallow(
+      <AppContext.Provider value={{ user: { isLoggedIn: true } }}>
+        <Footer />
+      </AppContext.Provider>
+    );
+    expect(wrapper.find("a[href='/contact']").exists()).toBe(true);
   });
 });
