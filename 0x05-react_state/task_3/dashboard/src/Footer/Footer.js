2c161d61421de5198c158b1637b40f31a4a97b7b
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-06 18:26:34 +0200

    Add contact link to Footer component

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./Footer.css";
 import { getFullYear, getFooterCopy } from "../utils/utils.js";
+import { AppContext } from "../App/AppContext.js";
 
 function Footer() {
-    return (
-        <div className="App-footer">
-          <p>
-            Copyright {getFullYear()} - {getFooterCopy(true)}
-          </p>
-        </div>
-    )
+  const { user } = useContext(AppContext);
+  return (
+    <div className="App-footer">
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+
+      {user.isLoggedIn && (
+        <p>
+          <a href="/contact">Contact us</a>
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default Footer;
