03535a384af0a9a102e14c5f02907ae74f0b7740
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-09 14:58:48 +0200

    the reducer is now using Map from immutable.js

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -3,38 +3,40 @@ import {
   DISPLAY_NOTIFICATION_DRAWER,
   SELECT_COURSE,
 } from "./actions/uiActionTypes";
+import { Map } from "immutable";
 
 describe("uiReducer", () => {
   it("should return the initial state when no action is passed", () => {
-    const initialState = {
+    const initialState = Map({
       isNotificationDrawerVisible: false,
       isUserLoggedIn: false,
-      user: {},
-    };
+      user: Map({}),
+    });
     expect(uiReducer(undefined, {})).toEqual(initialState);
   });
 
   it("should return the initial state when the action SELECT_COURSE is passed", () => {
-    const initialState = {
+    const initialState = Map({
       isNotificationDrawerVisible: false,
       isUserLoggedIn: false,
-      user: {},
-    };
+      user: Map({}),
+    });
     const action = { type: SELECT_COURSE };
     expect(uiReducer(initialState, action)).toEqual(initialState);
   });
 
   it("should correctly update isNotificationDrawerVisible property when DISPLAY_NOTIFICATION_DRAWER action is passed", () => {
-    const initialState = {
+    const initialState = Map({
       isNotificationDrawerVisible: false,
       isUserLoggedIn: false,
-      user: {},
-    };
+      user: Map({}),
+    });
     const action = { type: DISPLAY_NOTIFICATION_DRAWER };
-    const expectedState = {
-      ...initialState,
+    const expectedState = Map({
       isNotificationDrawerVisible: true,
-    };
+      isUserLoggedIn: false,
+      user: Map({}),
+    });
     expect(uiReducer(initialState, action)).toEqual(expectedState);
   });
 });
