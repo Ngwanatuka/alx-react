9bd91ba8ce250f14ab39a5c03a238cd451ef262e
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-09 15:32:51 +0200

    course readucers

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.js
@@ -0,0 +1,28 @@
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
+
+const initialState = new Map();
+
+export default function courseReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_COURSE_SUCCESS:
+      // Ensure that isSelected is set to false for every item in the data array
+      const coursesMap = new Map(action.data.map(course => [course.id, { ...course, isSelected: false }]));
+      return coursesMap;
+    case SELECT_COURSE:
+      return new Map([...state].map(([id, course]) => {
+        if (id === action.index) {
+          return [id, { ...course, isSelected: true }];
+        }
+        return [id, course];
+      }));
+    case UNSELECT_COURSE:
+      return new Map([...state].map(([id, course]) => {
+        if (id === action.index) {
+          return [id, { ...course, isSelected: false }];
+        }
+        return [id, course];
+      }));
+    default:
+      return state;
+  }
+}
