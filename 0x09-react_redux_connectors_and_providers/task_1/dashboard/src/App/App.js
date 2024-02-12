fe83fb8cc5ab71e7c2b744234b3338ff02bdee5b
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-12 22:01:13 +0200

    Refactor App component and import statements

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
@@ -1,63 +1,22 @@
-import Notifications from "../Notifications/Notifications";
 import React, { Component } from "react";
 import { StyleSheet, css } from "aphrodite";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
 import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
 import CourseList from "../CourseList/CourseList";
-import PropTypes from "prop-types";
 import BodySection from "../BodySection/BodySection";
 import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
 import { AppContext, user } from "./AppContext";
 import { getLatestNotification } from "../utils/utils";
-import { connect } from "react-redux";
-import { displayNotificationDrawer, hideNotificationDrawer } from "../actions/uiActionCreators";
-
-const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.uiReducer.get("isUserLoggedIn"),
-  };
-};
-
-const mapDispatchToProps = {
+import {
   displayNotificationDrawer,
   hideNotificationDrawer,
-};
+} from "../actions/uiActionCreators";
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayDrawer: false,
-      user: user,
-      logOut: this.logOut,
-      listNotifications: [
-        { id: 1, type: "default", value: "New course available" },
-        { id: 2, type: "urgent", value: "New resume available" },
-        { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-      ],
-    };
-    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
-    this.handleHideDrawer = this.handleHideDrawer.bind(this);
-    this.logIn = this.logIn.bind(this);
-    this.logOut = this.logOut.bind(this);
-    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
-  }
-
-  handleDisplayDrawer = () => {
-    this.setState({
-      displayDrawer: true,
-    });
-    this.props.displayNotificationDrawer();
-  };
-
-  handleHideDrawer = () => {
-    this.setState({
-      displayDrawer: false,
-    });
-    console.log("Close button has been clicked");
-    this.props.hideNotificationDrawer();
-  };
-
   logIn = (email, password) => {
     this.setState({
       user: {
@@ -89,28 +48,22 @@ class App extends Component {
   }
 
   render() {
+    const { isLoggedIn } =
+      this.props;
     const randomText =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget neque ornare, venenatis eros non, placerat elit.";
 
     return (
-      <AppContext.Provider
-        value={{ user: this.state.user, logOut: this.state.logOut }}
-      >
+      <AppContext.Provider value={{ user: user, logOut: this.logOut }}>
         <div className={css(styles.app)}>
-          <Notifications
-            listNotifications={this.state.listNotifications}
-            markNotificationAsRead={this.markNotificationAsRead}
-            displayDrawer={this.state.displayDrawer}
-            handleDisplayDrawer={this.handleDisplayDrawer}
-            handleHideDrawer={this.handleHideDrawer}
-          />
+          <Notifications />
           <div className={css(styles.appHeader)}>
             <Header />
           </div>
           <div className={css(styles.appBody)}>
-            {this.state.user.isLoggedIn ? (
+            {isLoggedIn ? (
               <BodySectionWithMarginBottom title="Course list">
-                <CourseList listCourses={this.listCourses} />
+                <CourseList />
               </BodySectionWithMarginBottom>
             ) : (
               <BodySectionWithMarginBottom title="Log in to continue">
@@ -130,7 +83,6 @@ class App extends Component {
   }
 }
 
-// Define styles using Aphrodite
 const styles = StyleSheet.create({
   app: {
     display: "flex",
@@ -155,14 +107,23 @@ const styles = StyleSheet.create({
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => {
-    return;
-  },
 };
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
+  displayNotificationDrawer: PropTypes.func.isRequired,
+  hideNotificationDrawer: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.uiReducer.get("isUserLoggedIn"),
+  };
+};
+
+const mapDispatchToProps = {
+  displayNotificationDrawer,
+  hideNotificationDrawer,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
